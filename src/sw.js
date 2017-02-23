af0b485505a2f0ee9b4d9f66ded974ed228674dc
chengpeng <chengpeng@jk.cn>
2017-02-23 11:55:51 +0800

    stage-2

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,21 @@
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open('cp-static-v1').then(cache => {
+			return cache.addAll([
+				'/',
+				'/static/js/bundle.js',
+				//'/static/media/logo.5d5d9eef.svg'
+			]);
+		})
+	);
+});
+
 self.addEventListener('fetch', event => {
-	// event.respondWith(
-	// 	new Response('hello world')
-	// );
+	event.respondWith(
+		caches.match(event.request).then( response => {
+			console.log(response);
+			if (response) return response;
+			return fetch(event.request);
+		})
+	);
 });
